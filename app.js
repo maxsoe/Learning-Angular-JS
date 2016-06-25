3c1bce13d6e641abcc65dbc25dc398fc4e950ae3
Max Soe <maxsoe@gmail.com>
2016-06-25 19:07:58 +0100

    1.3 Index HTML setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('store', []);
+// wrapping your javascript in a closure is a good habit.
+(function() {
+  var app = angular.module('store', []);
+
+  // notice that controller is attached to (inside) our app.
+  app.controller('StoreController', function(){
+    this.product = gem; // product is a property of controller
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'this is a one-of-a-kind dodecahedron',
+  }
+})();
